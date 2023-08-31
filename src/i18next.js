8b5d2f749e033bcdf29d72e08505e3e3f21ceb88
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-08-31 23:45:42 +0500

    other changes

--- src/i18next.js
+++ src/i18next.js
@@ -2,6 +2,7 @@ import i18next from "i18next";
 import {initReactI18next} from "react-i18next";
 import ru from "./locales/ru/transalation.json"
 import uz from "./locales/uz/transalation.json"
+import en from "./locales/en/transalation.json"
 import LanguageDetector from "i18next-browser-languagedetector"
 i18next 
 .use(initReactI18next)
@@ -12,11 +13,14 @@ i18next
       translation: uz
     },
     ru:{
-      translation:ru,
+      translation:ru
+    },
+    en:{
+      translation:en
     }
   },
   lng: window.localStorage.getItem('i18nextLng'),
-  fallbackLng: 'ru',
+  fallbackLng: 'uz',
   detection:{
     order:['htmlTag' , 'cookie', 'localStorage', 'path', 'subdomain'],
     caches:['cookie' , 'localStorage']
