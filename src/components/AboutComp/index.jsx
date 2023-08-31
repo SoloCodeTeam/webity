efdd977df23b69012aeda85a09697cd5b2d90005
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-08-31 23:48:57 +0500

    ok

--- src/components/AboutComp/index.jsx
+++ src/components/AboutComp/index.jsx
@@ -1,9 +1,11 @@
-
+import { useTranslation } from "react-i18next";
+import "./style.css"
 
 export function AboutComp() {
+    const { t, i18n } = useTranslation();
     return(
         <div className="AboutComp">
-         <h1></h1>
+            <h1>{t("about.0")}</h1>
         </div>
     )
  }
