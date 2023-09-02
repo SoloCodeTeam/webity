44bc5468aed34d56e8699d70c05517eda3333e44
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-09-02 19:15:52 +0500

    ok

--- src/components/AboutComp/index.jsx
+++ src/components/AboutComp/index.jsx
@@ -7,7 +7,30 @@ export function AboutComp() {
     return(
         <div className="AboutComp">
             <h1>{t("about.0")}</h1>
-            <GearAnimation  />
+            <GearAnimation/>
+            <ul>
+                <li>
+                <i class="fa-solid fa-users"></i>
+                    <span>
+                        <h2>{t("about.1")}</h2>
+                        <p>{t("about.2")}</p>
+                    </span>
+                </li>
+                <li>
+                <i class="fa-solid fa-pencil"></i>
+                    <span>
+                        <h2>{t("about.3")}</h2>
+                        <p>{t("about.4")}</p>
+                    </span>
+                </li>
+                <li>
+                <i class="fa-solid fa-trophy"></i>
+                    <span>
+                        <h2>{t("about.5")}</h2>
+                        <p>{t("about.6")}</p>
+                    </span>
+                </li>
+            </ul>
         </div>
     )
 }
