99e3e25ecb05885add0ed42bd4bf9f493ed969eb
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-09-03 00:10:56 +0500

    other changes

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,12 @@
 import { AboutComp } from "../../components/AboutComp";
+import { FooterComp } from "../../components/FooterComp";
 
 
 export function HomePage() {
    return(
        <div className="HomePage">
         <AboutComp/>
+        <FooterComp/>
        </div>
    )
 }
