483bea84acccb77a04be0d9b993b4bd6db8f9134
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-09-03 23:16:03 +0500

    added axios

--- src/components/MessageComp/index.jsx
+++ src/components/MessageComp/index.jsx
@@ -1,22 +1,54 @@
 import { useTranslation } from "react-i18next";
 import "./style.css"
 import { GearAnimation } from "../../utils";
+import { useRef } from "react";
+import axios from "axios";
 
 export function MessageComp() {
+    const name = useRef()
+    const message = useRef()
+    const phone = useRef()
     const { t, i18n } = useTranslation();
+    const telegram_bot_id = "5969874361:AAHtp0awChvZijejQ4eHr83qReplSmpyv2g";
+    const chat_id = 852144616;
+    let sendMessage;
+    const ready = function() {
+        sendMessage = "Ismi: " + name.current.value + "\nTelefon raqami: " + phone.current.value + "\nXabar: " + message.current.value;
+    };
+    const sendTelegram = () => {
+        ready();
+        // let settings = {
+        //     "async": true,
+        //     "crossDomain": true,
+        //     "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        //     "method": "POST",
+        //     "headers": {
+        //         "Content-Type": "application/json",
+        //         "cache-control": "no-cache"
+        //     },
+        //     "data": JSON.stringify({
+        //         "chat_id": chat_id,
+        //         "text": sendMessage
+        //     })
+        // };
+        axios.post("https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",{"chat_id": chat_id,"text":sendMessage})
+        name.current.value = "";
+        message.current.value = "";
+        phone.current.value = "";
+    };
     return(
         <div className="MessageComp">
             <h1>{t("message.0")}</h1>
             <GearAnimation/>
             <p>{t("message.1")}</p>
             <div className="MessageBox">
-                <form>
+                <form onSubmit={(e) => {e.preventDefault();sendTelegram();}}>
                     <h2>{t("message.3")}</h2>
                     <span>
-                        <input type="text" required placeholder={t("message.4")}/>
-                        <input type="text" required placeholder={t("message.5")}/>
+                        <input ref={name} type="text" required placeholder={t("message.4")}/>
+                        <input ref={phone} type="text" required placeholder={t("message.5")}/>
                     </span>
-                    <textarea required placeholder={t("message.6")}></textarea>
+                    <textarea required ref={message} placeholder={t("message.6")}></textarea>
                     <button type="submit"><i className="fa-solid fa-message"/>  {t("message.7")}</button>
                 </form>
                 <div className="LinksBox">
