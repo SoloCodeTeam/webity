80d56acb303763707a6101c7aaa79ad1eb6fba75
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-09-11 21:12:04 +0500

    other big changes

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,17 +5,10 @@ import { ProjectComp } from "../../components/ProjectComp";
 
 
 export function HomePage() {
-    document.cookie = "lang=uz"
-    console.log(document.cookie);
-    const button =() => {
-        document.cookie = "lang=ru"
-        console.log(document.cookie.indexOf("lang"));
-    }
    return(
        <div className="HomePage">
         <AboutComp/>
         <ProjectComp/>
-        <button onClick={button}>nivec</button>
         <MessageComp/>
         <FooterComp/>
        </div>
