dfa73bacc14a494eb439d1e1a3d6e69929d21a76
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-09-12 16:09:02 +0500

    ok

--- src/locales/index.jsx
+++ src/locales/index.jsx
@@ -7,9 +7,9 @@ export function LangSelect() {
     const LanBtnCls = useRef();
     const langug = useRef();
     const LangSel = (e) => {
-        if(e.target.id == "uz") {window.localStorage.setItem('lang','🇺🇿');document.cookie="uz"}
-        if(e.target.id == "en") {window.localStorage.setItem('lang','🇺🇸');document.cookie="en"}
-        if(e.target.id == "ru") {window.localStorage.setItem('lang','🇷🇺');document.cookie="ru"}
+        if(e.target.id == "uz") {window.localStorage.setItem('lng','uz');document.cookie="uz"}
+        if(e.target.id == "en") {window.localStorage.setItem('lng','en');document.cookie="en"}
+        if(e.target.id == "ru") {window.localStorage.setItem('lng','ru');document.cookie="ru"}
         LangList.current.style.transform = "scale(0)"
         LanBtn.current.style.display = "flex"
         LanBtnCls.current.style.display = "none"
@@ -30,9 +30,9 @@ export function LangSelect() {
             <button className='LangBtn' ref={LanBtn} onClick={LangBtn}>{window.localStorage.getItem('lang')}<i className='fa-solid fa-caret-down'></i></button>
             <button className='LangBtnCls' ref={LanBtnCls} onClick={LangBtnCls}>{window.localStorage.getItem('lang')}<i className='fa-solid fa-caret-up'></i></button>
             <div className="LangList" ref={LangList}>
-                <button onClick={LangSel} id="uz" ref={langug}>Uzb 🇺🇿</button>
-                <button onClick={LangSel} id="en" ref={langug}>Eng 🇺🇸</button>
-                <button onClick={LangSel} id="ru" ref={langug}>Rus 🇷🇺</button>
+                <button onClick={LangSel} id="uz" ref={langug}>🇺🇿</button>
+                <button onClick={LangSel} id="en" ref={langug}>🇺🇸</button>
+                <button onClick={LangSel} id="ru" ref={langug}>🇷🇺</button>
             </div>
         </div>
     )
